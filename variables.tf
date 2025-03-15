c698cfb22ff0b0a599d0c3e8f1a05bdc889603ae
Henrique Fukuya <henriquefukuya@gmail.com>
2025-03-15 14:54:21 -0300

    Testando Pipeline v3

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
+variable "client_id" {
+  description = "O Client ID do Service Principal do Azure"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "O Client Secret do Service Principal do Azure"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "O Subscription ID da conta Azure"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "O Tenant ID da conta Azure"
+  type        = string
+}
