8166877eabd25007fb6044c342a867dfdc5da44f
Henrique Fukuya <henriquefukuya@gmail.com>
2025-03-15 16:02:53 -0300

    Testando Pipeline v6

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
-  features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  features {}  # Não é necessário fornecer as credenciais aqui
 }
 
 resource "azurerm_resource_group" "rg" {
