d9a6438af6ea6394905e0743100bb3fb53419b93
Henrique Fukuya <henriquefukuya@gmail.com>
2025-03-16 21:39:32 -0300

    Testando pipeline v8

--- main.tf
+++ main.tf
@@ -1,64 +1,85 @@
+
 provider "azurerm" {
-  features {}  # Não é necessário fornecer as credenciais aqui
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform"
-  location = "East US"
+/*
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-stage-azure-open-ai"
+    storage_account_name = "terraformstateopen2024ai"
+    container_name       = "terraformopenai2024"
+    key                  = "terraform.tfstate"
+  }
+}*/
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources5"
+  location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-terraform"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network3"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-terraform"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet3"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-terraform"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic3"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name                          = "internal2"
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-terraform"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [azurerm_network_interface.nic.id]
+resource "azurerm_virtual_machine" "example" {
+  name                  = "example-machine3"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = "Standard_DS1_v2"
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")  # Certifique-se de ter sua chave SSH gerada
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
-}
 
-output "vm_public_ip" {
-  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
+  storage_os_disk {
+    name              = "example-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "example-machine"
+    admin_username = "adminuser"
+    admin_password = "AdminPassword123!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
